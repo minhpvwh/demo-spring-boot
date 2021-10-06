fa8f5438dae24bcd52c6fb7f1fd64aa2be2caaf5
minhpvwh <wh.pvminh@gmail.com>
2021-10-06 16:58:01 +0700

    update

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -15,16 +15,14 @@ public class StudentConfig {
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
             Student std1 = new Student(
-                    1L,
                     "Mariam",
-                    "minhpv@gmail.com",
+                    "minhpv123@gmail.com",
                     LocalDate.of(2000, Month.APRIL, 4)
             );
             Student std2 = new Student(
-                    1L,
                     "Mariam",
-                    "minhpv@gmail.com",
-                    LocalDate.of(2000, Month.APRIL, 4)
+                    "minhp789v@gmail.com",
+                    LocalDate.of(2002, Month.APRIL, 4)
             );
 
             repository.saveAll(List.of(std1, std2));
